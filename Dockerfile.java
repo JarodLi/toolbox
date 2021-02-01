bf250a92b1b519e98ccfbabb49a2e1b96c4dd6ae
JarodLi <jarod.lijian@gmail.com>
2021-02-01 22:16:04 +0800

    add tomcat9

--- Dockerfile.java
+++ Dockerfile.java
@@ -8,7 +8,7 @@ COPY pkgs/java/eclipse.jdt.ls /opt/eclipse.jdt.ls
 RUN mkdir -p /opt/eclipse.jdt.ls/org.eclipse.jdt.ls.product/target/repository
 ADD pkgs/java/jdt-language-server-latest.tar.gz /opt/eclipse.jdt.ls/org.eclipse.jdt.ls.product/target/repository
 
-RUN pacman -S maven --noconfirm
+RUN pacman -S maven tomcat9 --noconfirm
 
 RUN cd /opt &&  wget https://github.com/google/google-java-format/releases/download/google-java-format-1.9/google-java-format-1.9-all-deps.jar
 RUN echo "java -jar /opt/google-java-format-1.9-all-deps.jar -r \$@" > /usr/bin/google-java-format && chmod +x /usr/bin/google-java-format
