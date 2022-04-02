96ef3f5c8be40945232521eadcdc90e864d08d0b
JarodLi <jarod.lijian@gmail.com>
2022-04-02 14:52:32 +0800

    Create init.lua

---
+++ config/vim/init.lua
@@ -0,0 +1,16 @@
+-- 基础设置
+require('basic')
+require('keybindings')
+
+-- Packer 插件管理
+require("plugins")
+
+-- 主题设置 （新增）
+require("colorscheme")
+
+-- 插件配置
+require("plugin-config.nvim-tree")
+
+require("plugin-config.bufferline")
+require("plugin-config.lualine")
+require("plugin-config.telescope")
