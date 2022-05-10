afc1757945555f79da52676bf291799d96018ab4
JarodLi <jarod.lijian@gmail.com>
2022-05-10 21:42:01 +0800

    Create vim-smoothie.lua

---
+++ config/nvim/lua/plugin-config/vim-smoothie.lua
@@ -0,0 +1,4 @@
+vim.cmd('nnoremap <unique> <C-D> <cmd>call smoothie#do("9j") <CR>')                                                                   
+vim.cmd('nnoremap <unique> <C-U> <cmd>call smoothie#do("9k") <CR>')
+vim.cmd('nnoremap <unique> <C-j> <cmd>call smoothie#do("5j") <CR>')
+vim.cmd('nnoremap <unique> <C-k> <cmd>call smoothie#do("5k") <CR>')
