0d31db71e9359030fba0b14082c1c46d44982077
JarodLi <jarod.lijian@gmail.com>
2022-05-12 17:04:23 +0800

    Update keybindings.lua

--- config/nvim/lua/keybindings.lua
+++ config/nvim/lua/keybindings.lua
@@ -115,27 +115,26 @@ pluginKeys.nvimTreeList = {
 }
 
 -- Telescope
--- 查找文件
-map("n", "<leader>ff", "<cmd>Telescope find_files theme=dropdown<CR>", opt)
--- 查找文字
-map("n", "<leader>fg", "<cmd>Telescope live_grep theme=dropdown<CR>", opt)
--- 查找当前buffer内容
-map("n", "<leader>fb", "<cmd>Telescope current_buffer_fuzzy_find theme=dropdown<CR>", opt)
--- 查找帮助文档
-map("n", "<leader>fh", "<cmd>Telescope help_tags theme=dropdown<CR>", opt)
--- 查找最近打开的文件
-map("n", "<leader>fo", "<cmd>Telescope oldfiles theme=dropdown<CR>", opt)
--- 查找 marks 标记
-map("n", "<leader>fm", "<cmd>Telescope marks theme=dropdown<CR>", opt)
+ -- 查找当前buffer内容
+   map("n", "<leader>fb", "<cmd>Telescope current_buffer_fuzzy_find theme=dropdown<CR>", opt)
+   -- 查找帮助文档
+   map("n", "<leader>fh", "<cmd>Telescope help_tags theme=dropdown<CR>", opt)
+   -- 查找最近打开的文件
+   map("n", "<leader>fo", "<cmd>Telescope oldfiles theme=dropdown<CR>", opt)
+   -- 查找 marks 标记
+   map("n", "<leader>fm", "<cmd>Telescope marks theme=dropdown<CR>", opt)
+   map("n", "<leader>fs", "<cmd>Telescope treesitter<CR>", opt)
+   vim.cmd("nmap <leader>ff :Telescope ")
 
-map("n", "<leader>fp", ":lua require'telescope'.extensions.project.project{}<CR>", opt)
+   map("n", "<leader>fp", ":lua require'telescope'.extensions.project.project{}<CR>", opt)
 
--- 查找文件
-map("n", "<C-p>", ":Telescope find_files<CR>", opt)
--- 全局搜索
-map("n", "<C-f>", ":Telescope live_grep<CR>", opt)
-map("n", "<A-f>", ":Telescope grep_string<CR>", opt)
---map("n", "<leader>bf", ":Telescope buffers<CR>", opt)
+   -- 查找文件
+   map("n", "<C-p>", ":Telescope find_files<CR>", opt)
+   -- 全局搜索
+   map("n", "<C-f>", ":Telescope live_grep<CR>", opt)
+   map("n", "<A-f>", ":Telescope grep_string<CR>", opt)
+
+
 -- Telescope 列表中 插入模式快捷键
 pluginKeys.telescopeList = {
 	i = {
