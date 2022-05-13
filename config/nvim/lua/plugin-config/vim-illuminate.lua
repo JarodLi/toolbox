a95d092568482a4abd7ed3ac47485f0fe0801aae
JarodLi <jarod.lijian@gmail.com>
2022-05-13 18:53:59 +0800

    Update vim-illuminate.lua

--- config/nvim/lua/plugin-config/vim-illuminate.lua
+++ config/nvim/lua/plugin-config/vim-illuminate.lua
@@ -8,3 +8,10 @@ vim.g.Illuminate_ftblacklist = {
     "vista_kind",
     "toggleterm"
 }
+ vim.api.nvim_set_keymap("n", "<a-n>", '<cmd>lua require"illuminate".next_reference{wrap=true}<cr>', { noremap = true })
+ vim.api.nvim_set_keymap(
+    "n",
+    "<a-p>",
+    '<cmd>lua require"illuminate".next_reference{reverse=true,wrap=true}<cr>',
+    { noremap = true }
+ )
