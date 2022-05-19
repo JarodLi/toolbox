923f022f9815150f3ea9c2b12a8e47a90172b6fd
JarodLi <jarod.lijian@gmail.com>
2022-05-19 20:40:36 +0800

    Update dashboard.lua

--- config/nvim/lua/plugin-config/dashboard.lua
+++ config/nvim/lua/plugin-config/dashboard.lua
@@ -1,23 +1,37 @@
-vim.g.dashboard_default_executive = "telescope"                                                  
-vim.g.dashboard_custom_footer = { "Hello World!    " }                                                                     
-                                                                                                    
-vim.g.dashboard_custom_section = {                                                                               
-    a = { description = { "    Projects              " }, command = "Telescope project" },        
-    b = { description = { "    Recently files        " }, command = "Telescope oldfiles" },
-    -- c = { description = { "  Edit keybindings      " }, command = "edit ~/.config/nvim/lua/keybindings.lua" },
-    -- d = { description = { "    Edit Projects         " }, command = "edit ~/.local/share/nvim/project_nvim/project_history", },
-    -- e = { description = { "  Edit .bashrc          " }, command = "edit ~/.bashrc" },
-    -- f = { description = { "  Edit init.lua         " }, command = "edit ~/.config/nvim/init.lua" },
-    g = { description = { "    Find file            " }, command = "Telescope find_files" },
-    h = { description = { "    Find text            " }, command = "Telescope live_grep" },
+vim.g.dashboard_default_executive = "telescope"
+vim.g.dashboard_custom_footer = { "Hello World!    " }
+
+vim.g.dashboard_custom_section = {
+	-- a = {
+	-- 	description = { "   Open last session          " },
+	-- 	command = " RestoreSession",
+	-- },
+	a = { description = { "    Recently files        " }, command = "Telescope oldfiles" },
+	b = { description = { "    Projects              " }, command = "Telescope project" },
+	-- c = { description = { "  Edit keybindings      " }, command = "edit ~/.config/nvim/lua/keybindings.lua" },
+	-- d = { description = { "    Edit Projects         " }, command = "edit ~/.local/share/nvim/project_nvim/project_history", },
+	c = {
+		description = { "    Edit Projects         " },
+		command = "edit /root/.local/share/nvim/telescope-projects.txt",
+	},
+	d = {
+		description = { "    Jump to bookmarks     " },
+		command = "Telescope marks",
+	},
+	e = { description = { "    Change colorscheme    " }, command = "Telescope colorscheme" },
+	-- e = { description = { "  Edit .bashrc          " }, command = "edit ~/.bashrc" },
+	-- f = { description = { "  Edit init.lua         " }, command = "edit ~/.config/nvim/init.lua" },
+	f = { description = { "    Find file            " }, command = "Telescope find_files" },
+	g = { description = { "    Find text            " }, command = "Telescope live_grep" },
+	-- i = { description = { "   New file            " }, command = "Telescope live_grep" },
 }
 
 vim.g.dashboard_custom_header = {
-    [[ ███╗   ███╗███████╗███████╗██████╗  ██████╗  ]],
-    [[ ████╗ ████║██╔════╝██╔════╝██╔══██╗██╔═══██╗ ]],
-    [[ ██╔████╔██║█████╗  █████╗  ██████╔╝██║   ██║ ]],
-    [[ ██║╚██╔╝██║██╔══╝  ██╔══╝  ██╔═══╝ ██║   ██║ ]],
-    [[ ██║ ╚═╝ ██║███████╗███████╗██║     ╚██████╔╝ ]],
-    [[ ╚═╝     ╚═╝╚══════╝╚══════╝╚═╝      ╚═════╝  ]],
+	[[ ███╗   ███╗███████╗███████╗██████╗  ██████╗  ]],
+	[[ ████╗ ████║██╔════╝██╔════╝██╔══██╗██╔═══██╗ ]],
+	[[ ██╔████╔██║█████╗  █████╗  ██████╔╝██║   ██║ ]],
+	[[ ██║╚██╔╝██║██╔══╝  ██╔══╝  ██╔═══╝ ██║   ██║ ]],
+	[[ ██║ ╚═╝ ██║███████╗███████╗██║     ╚██████╔╝ ]],
+	[[ ╚═╝     ╚═╝╚══════╝╚══════╝╚═╝      ╚═════╝  ]],
 }
 -- vim.cmd("autocmd FileType dashboard set laststatus=0")
