4c2a2f2f0f70d3bbb511d412366d742dd3f6b5b9
JarodLi <jarod.lijian@gmail.com>
2022-05-20 18:00:49 +0800

    Update python.lua

--- config/nvim/ftplugin/python.lua
+++ config/nvim/ftplugin/python.lua
@@ -10,5 +10,7 @@ vim.bo.expandtab = true
 
 vim.o.autoindent = true
 vim.bo.autoindent = true
+-- 不能开启smartindent，否则py代码第一行#开头的注释无法对齐
+-- vim.o.smartindent = true
 -- vim.opt.listchars:append("space:.")
 -- vim.opt.listchars:append("eol:↴")
