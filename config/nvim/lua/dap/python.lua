9f14368e2607f5e9bfd91bc7579429ba669be4e7
JarodLi <jarod.lijian@gmail.com>
2022-05-21 15:44:41 +0800

    Update python.lua

--- config/nvim/lua/dap/python.lua
+++ config/nvim/lua/dap/python.lua
@@ -1,4 +1,12 @@
 -- python3 -m pip install debugpy
+function Split(s, delimiter)                                    
+    local result = {}                                           
+    for match in (s .. delimiter):gmatch("(.-)" .. delimiter) do
+        table.insert(result, match)                             
+    end                                                         
+    return result
+end
+
 return {
 	adapters = {
 		type = "executable",
@@ -14,6 +22,17 @@ return {
 			pythonPath = function()
 				return vim.g.python_path
 			end,
+			 -- buildFlags = function()
+			 --  local tags = vim.fn.input("Build tags: ")
+			 --  return tags
+			 -- end,
+			 -- args = { "-m", "sql", "-c", "0" },
+			 -- 支持传入参数，需要F9重启一次才能输入
+			 args = function()
+			     local args = vim.fn.input("run args: ")
+			     return Split(args, " ")
+			     -- local args_lst = vim.luaeval(args)
+			 end,
 		},
 	},
 }
