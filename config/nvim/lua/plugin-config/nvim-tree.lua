60321b126ebe0c8ee451a00552e8bb64adb87420
JarodLi <jarod.lijian@gmail.com>
2023-07-18 19:33:29 +0800

    Update nvim-tree.lua

--- config/nvim/lua/plugin-config/nvim-tree.lua
+++ config/nvim/lua/plugin-config/nvim-tree.lua
@@ -7,15 +7,67 @@ end
 -- 列表操作快捷键
 local list_keys = require("keybindings").nvimTreeList
 
-nvim_tree.setup({
+local function on_attach(bufnr)
+  local api = require('nvim-tree.api')
+
+  local function opts(desc)
+    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+
+  -- Default mappings. Feel free to modify or remove as you wish.
+  --
+  -- BEGIN_DEFAULT_ON_ATTACH
+  -- END_DEFAULT_ON_ATTACH
+
+
+  -- Mappings removed via:
+  --   remove_keymaps
+  --   OR
+  --   view.mappings.list..action = ""
+  --
+  -- The dummy set before del is done for safety, in case a default mapping does not exist.
+  --
+  -- You might tidy things by removing these along with their default mapping.
+  vim.keymap.set('n', '<Tab>', '', { buffer = bufnr })
+  vim.keymap.del('n', '<Tab>', { buffer = bufnr })
+  vim.keymap.set('n', '<C-k>', '', { buffer = bufnr })
+  vim.keymap.del('n', '<C-k>', { buffer = bufnr })
+
+
+  -- Mappings migrated from view.mappings.list
+  --
+  -- You will need to insert "your code goes here" for any mappings with a custom action_cb
+  vim.keymap.set('n', '<CR>', api.node.open.edit, opts('Open'))
+  vim.keymap.set('n', 'o', api.node.open.edit, opts('Open'))
+  vim.keymap.set('n', '<2-LeftMouse>', api.node.open.edit, opts('Open'))
+  vim.keymap.set('n', 'v', api.node.open.vertical, opts('Open: Vertical Split'))
+  vim.keymap.set('n', 'h', api.node.open.horizontal, opts('Open: Horizontal Split'))
+  vim.keymap.set('n', 'i', api.tree.toggle_gitignore_filter, opts('Toggle Git Ignore'))
+  vim.keymap.set('n', '.', api.tree.toggle_hidden_filter, opts('Toggle Dotfiles'))
+  vim.keymap.set('n', '<F5>', api.tree.reload, opts('Refresh'))
+  vim.keymap.set('n', 'A', api.fs.create, opts('Create'))
+  vim.keymap.set('n', 'D', api.fs.remove, opts('Delete'))
+  vim.keymap.set('n', 'r', api.fs.rename, opts('Rename'))
+  vim.keymap.set('n', 'x', api.fs.cut, opts('Cut'))
+  vim.keymap.set('n', 'c', api.fs.copy.node, opts('Copy'))
+  vim.keymap.set('n', 'p', api.fs.paste, opts('Paste'))
+  vim.keymap.set('n', 's', api.node.run.system, opts('Run System'))
+  vim.keymap.set('n', '-', api.tree.change_root_to_parent, opts('Up'))
+  vim.keymap.set('n', '`', api.node.open.preview, opts('Open Preview'))
+  vim.keymap.set('n', '<C-l>', api.node.show_info_popup, opts('Info'))
+
+end
+
+local opts={
 	auto_reload_on_write = true,
 	disable_netrw = false,
 	hijack_cursor = false,
 	hijack_netrw = true,
 	hijack_unnamed_buffer_when_opening = false,
-	ignore_buffer_on_setup = false,
-	open_on_setup = false,
-	open_on_setup_file = false,
+	-- ignore_buffer_on_setup = false,
+	-- open_on_setup = false,
+	-- open_on_setup_file = false,
 	open_on_tab = false,
 	sort_by = "name",
 	update_cwd = true,
@@ -25,7 +77,7 @@ nvim_tree.setup({
 	},
 	view = {
 		width = 30,
-		height = 30,
+		-- height = 30,
 		hide_root_folder = false,
 		side = "right",
 		preserve_window_proportions = false,
@@ -54,7 +106,7 @@ nvim_tree.setup({
 		enable = true,
 		auto_open = true,
 	},
-	ignore_ft_on_setup = {},
+	-- ignore_ft_on_setup = {},
 	system_open = {
 		cmd = "",
 		args = {},
@@ -90,7 +142,7 @@ nvim_tree.setup({
 			-- 首次打开大小适配
 			resize_window = true,
 			-- 打开文件时关闭
-			quit_on_open = true,
+			quit_on_open = false,
 			window_picker = {
 				enable = true,
 				chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
@@ -117,8 +169,11 @@ nvim_tree.setup({
 			profile = false,
 		},
 	},
-})
+    on_attach=on_attach,
+}
+nvim_tree.setup(opts)
+
 -- 自动关闭
--- vim.cmd([[
---   autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif
--- ]])
+vim.cmd([[
+  autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif
+]])
